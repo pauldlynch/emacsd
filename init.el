7787572c975915174a60c5802c63a733d92b67b0
Paul Lynch <paul@plsys.co.uk>
2019-06-03 11:19:52 +0100

    Setting up git for .emacs.d repository.

---
+++ init.el
@@ -0,0 +1,39 @@
+;; set up packages package
+(require 'package)
+(setq package-enable-at-startup nil)
+(setq package-archives '( ("gnu" . "https://elpa.gnu.org/packages/")
+			  ("marmalade" . "https://marmalade-repo.org/packages/")
+			  ("melpa" . "http://melpa.org/packages/")
+			  ;; ("org" . "http://orgmode.org/elpa/")
+			  ))
+(package-initialize)
+
+;; Bootstrap use-package
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(eval-when-compile
+  (require 'use-package))
+(setq use-package-always-ensure t)
+
+(org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))
+
+;; M-x customize mess
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("3629b62a41f2e5f84006ff14a2247e679745896b5eaa1d5bcfbc904a3441b0cd" "9492cf1ac00c8a1f7130a867a97404dfeb6727801c6b2b40b853b91543f7af67" "40f6a7af0dfad67c0d4df2a1dd86175436d79fc69ea61614d668a635c2cd94ab" "6df30cfb75df80e5808ac1557d5cc728746c8dbc9bc726de35b15180fa6e0ad9" "bcc6775934c9adf5f3bd1f428326ce0dcd34d743a92df48c128e6438b815b44f" default)))
+ '(nrepl-message-colors
+   (quote
+    ("#CC9393" "#DFAF8F" "#F0DFAF" "#7F9F7F" "#BFEBBF" "#93E0E3" "#94BFF3" "#DC8CC3"))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
