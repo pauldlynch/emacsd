70026dce18aef4d2317dabb005d36057f30bb932
Paul Lynch <paul@plsys.co.uk>
2019-06-18 20:32:00 +0100

    init.el updated by emacs.

--- init.el
+++ init.el
@@ -29,11 +29,19 @@
     ("3629b62a41f2e5f84006ff14a2247e679745896b5eaa1d5bcfbc904a3441b0cd" "9492cf1ac00c8a1f7130a867a97404dfeb6727801c6b2b40b853b91543f7af67" "40f6a7af0dfad67c0d4df2a1dd86175436d79fc69ea61614d668a635c2cd94ab" "6df30cfb75df80e5808ac1557d5cc728746c8dbc9bc726de35b15180fa6e0ad9" "bcc6775934c9adf5f3bd1f428326ce0dcd34d743a92df48c128e6438b815b44f" default)))
  '(nrepl-message-colors
    (quote
-    ("#CC9393" "#DFAF8F" "#F0DFAF" "#7F9F7F" "#BFEBBF" "#93E0E3" "#94BFF3" "#DC8CC3"))))
+    ("#CC9393" "#DFAF8F" "#F0DFAF" "#7F9F7F" "#BFEBBF" "#93E0E3" "#94BFF3" "#DC8CC3")))
+ '(org-default-notes-file (concat org-directory "/notes.org"))
+ '(org-directory "~/Dropbox/orgfiles")
+ '(org-export-html-postamble nil)
+ '(org-hide-leading-stars t)
+ '(org-startup-folded (quote overview))
+ '(org-startup-indented t)
+ '(package-selected-packages
+   (quote
+    (markdown-mode typo olivetti poet-theme company paredit rainbow-delimiters treemacs-projectile treemacs dumb-jump counsel-projectile projectile web-mode iedit expand-region hungry-delete undo-tree yasnippet-snippets yasnippet flycheck htmlize ox-reveal flx smex counsel ivy org-ac org-bullets which-key try moe-theme powerline color-theme diminish use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
-
